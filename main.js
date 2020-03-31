8a6993eece11c668c25d2ad8994a06f525512a08
kyawmoehan <kyawmoehan012@gmail.com>
2020-03-31 21:42:55 +0630

    expand

---
+++ main.js
@@ -0,0 +1,17 @@
+    var coll = document.querySelectorAll("#myBtn");
+    coll.forEach(item => {
+        item.addEventListener('click', (e) => {
+            const dots = item.parentElement.querySelector('#dots');
+            const par = item.parentElement.querySelector('#more');
+            const btnText = item.parentElement.querySelector("#myBtn");
+            if (dots.style.display === "none") {
+                dots.style.display = "inline";
+                btnText.innerHTML = "⌄"; 
+                par.style.display = "none";
+            } else {
+                dots.style.display = "none";
+                btnText.innerHTML = "⌃"; 
+                par.style.display = "inline";
+            }   
+        });
+    });
