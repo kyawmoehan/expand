5375da966e33d0d8c2a0fe701130d7e0f0a104cd
kyawmoehan <kyawmoehan012@gmail.com>
2020-04-01 12:01:22 +0630

    change to onclick function

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-    var coll = document.querySelectorAll("#myBtn");
-    coll.forEach(item => {
-        item.addEventListener('click', (e) => {
-            const dots = item.parentElement.querySelector('#dots');
-            const par = item.parentElement.querySelector('#more');
-            const btnText = item.parentElement.querySelector("#myBtn");
-            if (dots.style.display === "none") {
-                dots.style.display = "inline";
-                btnText.innerHTML = "⌄"; 
-                btnText.style.display = "inline"; 
-                par.style.display = "none";
-            } else {
-                dots.style.display = "none";
-                btnText.innerHTML = "⌃";
-                btnText.style.display = "block"; 
-                par.style.display = "inline";
-            }   
-        });
-    });
+    // var coll = document.querySelectorAll("#myBtn");
+    // coll.forEach(item => {
+    //     item.addEventListener('click', (e) => {
+    //         const dots = item.parentElement.querySelector('#dots');
+    //         const par = item.parentElement.querySelector('#more');
+    //         const btnText = item.parentElement.querySelector("#myBtn");
+    //         if (dots.style.display === "none") {
+    //             dots.style.display = "inline";
+    //             btnText.innerHTML = "⌄"; 
+    //             btnText.style.display = "inline"; 
+    //             par.style.display = "none";
+    //         } else {
+    //             dots.style.display = "none";
+    //             btnText.innerHTML = "⌃";
+    //             btnText.style.display = "block"; 
+    //             par.style.display = "inline";
+    //         }   
+    //     });
+    // });
+
+
+    // using on click
+    function expand(e) {
+        const item = e;
+        const dots = item.parentElement.querySelector('#dots');
+        const par = item.parentElement.querySelector('#more');
+        const btnText = item.parentElement.querySelector("#myBtn");
+        if (dots.style.display === "none") {
+            dots.style.display = "inline";
+            btnText.innerHTML = "⌄"; 
+            btnText.style.display = "inline"; 
+            par.style.display = "none";
+        } else {
+            dots.style.display = "none";
+            btnText.innerHTML = "⌃";
+            btnText.style.display = "block"; 
+            par.style.display = "inline";
+        }   
+    }
